f89a11588bc9de1fe34044407065e86f742c4bd5
Travis-Gray2194 <travisogray1@gmail.com>
2018-02-07 14:40:56 -0500

    --------Commit 5 Changelog----------- Time:2:39pm Date:Wednesday Feb 7th,2018 Added Thymeleaf Footer and Customized Stylesheet NavBar and Footer Display correctly on all pages

---
+++ src/main/java/me/travisgray/demo/RecipeController.java
@@ -0,0 +1,72 @@
+package me.travisgray.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+
+    @RequestMapping("/")
+    public String showindex(){
+        return "index";
+    }
+
+    @RequestMapping("/PumpkinRice")
+    public String showpumpkinrice(){
+        return "pumpkinrice";
+    }
+
+
+    @RequestMapping("/JerkChicken")
+    public String showjerkchicken(){
+        return "jerkchicken";
+    }
+
+    @RequestMapping("/CrabCake")
+    public String showcrabcake(){
+        return "crabcake";
+    }
+
+    @RequestMapping("/Injera")
+    public String showInjera(){
+        return "injera";
+    }
+
+    @RequestMapping("/Makroudh")
+    public String showMakroudh(){
+        return "makroudh";
+    }
+
+    @RequestMapping("/EggSauce")
+    public String showEggSauce(){
+        return "eggsauce";
+    }
+
+    @RequestMapping("/GulabJamun")
+    public String showGulabJamun(){
+        return "gulabjamun";
+    }
+
+
+    @RequestMapping("/Croquemadame")
+    public String croquemadame(){
+        return "croquemadame";
+    }
+
+    @RequestMapping("/Gallery")
+    public String showGallery(){
+        return "gallery";
+    }
+
+    @RequestMapping("/Wings")
+    public String showChineseWings(){
+        return "wings";
+    }
+
+    @RequestMapping("/Bara")
+    public String showBara(){
+        return "bara";
+    }
+
+
+}
